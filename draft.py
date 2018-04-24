8b8f57498856d35dfccbf13d7222bbc54dcf349d
«Grigorii-P» <pogoreloff.g@gmail.com>
2018-04-24 13:22:16 +0300

    3 lanes and 5000 background cars added

--- draft.py
+++ draft.py
@@ -15,17 +15,47 @@ from random import uniform
 import xml.etree.ElementTree
 from utils import *
 
-folder_sh = '/Users/grigoriipogorelov/Desktop/RA_project/RA_rand_path_planning/maps/melbourne_sh/results_sh_'
-folder_a = '/Users/grigoriipogorelov/Desktop/RA_project/RA_rand_path_planning/maps/melbourne_a/results_a_'
-num_vehicles = ['50','100','200','500','1000','1500','2000']
-shortest_paths = []
-a_paths = []
-for n in num_vehicles:
-    path_sh = folder_sh + n + '.xml'
-    path_a = folder_a + n + '.xml'
-    mean_short = mean_time(path_sh)
-    mean_a = mean_time(path_a)
-    shortest_paths.append(mean_short)
-    a_paths.append(mean_a)
+filename = '/Users/grigoriipogorelov/Desktop/melbourne.osm'
+# filename = '/Users/grigoriipogorelov/Desktop/inno.osm'
+g = osmgraph.parse_file(filename)
 
-print
+valid_nodes = {}
+for n1, n2 in g.edges():
+    valid_nodes[n1] = 0
+    valid_nodes[n2] = 0
+    c1, c2 = osmgraph.tools.coordinates(g, (n1, n2))
+    g[n1][n2]['length'] = geog.distance(c1, c2)
+
+a_star = A_star(g, valid_nodes)
+
+
+
+num_cars = [50,100,200,500,1000,1500,2000]
+num_cars_back = 5000
+type_cars_variety = 20
+
+sources = [370708433]
+destinations = [1928298965]
+
+shortest_paths, a_paths = [], []
+for src, dst in itertools.izip(sources, destinations):
+    path = nx.shortest_path(g, src, dst, 'length')
+    shortest_paths.append(path)
+
+k = 2
+range_ = 10
+k_max = k
+step = (k_max - 1) / range_
+k_range = list(np.arange(1, k_max, step))
+k_range.append(k_max)
+
+variety = 20
+a_star = A_star(g, valid_nodes)
+for src, dst in itertools.izip(sources, destinations):
+    for i in range(variety):
+        a_star_path, _ = a_star.a_star_search(src, dst, k_range)
+        a_paths.append(a_star_path)
+
+for n_cars in num_cars:
+    all_xmls(g, shortest_paths, type_cars_variety, n_cars, num_cars_back, True, 'maps/melbourne_sh/melbourne_sh_' + str(n_cars))
+    all_xmls(g, a_paths, type_cars_variety, n_cars, num_cars_back, False, 'maps/melbourne_a/melbourne_a_' + str(n_cars))
--- xml_creation.py
+++ xml_creation.py
@@ -25,24 +25,6 @@ def nodes_xml(graph, paths, file_name):
     tree = ET.ElementTree(nodes)
     tree.write(file_name + ".nod.xml")
 
-    xml_ = xml.dom.minidom.parseString(tostring(nodes))
-    # print xml_.toprettyxml()
-
-def single_edges_xml(path, file_name):
-    edges = Element('edges')
-    first = path[0]
-    for i in range(1, len(path)):
-        from_ = str(first)
-        to_ = str(path[i])
-        id = from_+'_'+to_
-        first = path[i]
-        edge = SubElement(edges, 'edge', {'from': from_, 'id': id, 'to': to_})
-    tree = ET.ElementTree(edges)
-    tree.write(file_name + ".edg.xml")
-
-    xml_ = xml.dom.minidom.parseString(tostring(edges))
-    # print xml_.toprettyxml()
-
 def multiple_edges_xml(paths, file_name):
     edges = Element('edges')
 
@@ -53,7 +35,7 @@ def multiple_edges_xml(paths, file_name):
             to_ = str(path[i])
             id = from_ + '_' + to_
             first = path[i]
-            edge = SubElement(edges, 'edge', {'from': from_, 'id': id, 'to': to_})
+            edge = SubElement(edges, 'edge', {'from': from_, 'id': id, 'to': to_, 'type':'a'})
 
     tree = ET.ElementTree(edges)
     tree.write(file_name + ".edg.xml")
@@ -61,49 +43,9 @@ def multiple_edges_xml(paths, file_name):
     xml_ = xml.dom.minidom.parseString(tostring(edges))
     # print xml_.toprettyxml()
 
-def single_route_vehicles_xml(path, num_vehicle_types, num_cars, file_name):
-    num_cars_in_flow = num_cars
-    routes = Element('routes')
-
-    for i in range(num_vehicle_types):
-        accel = str(round(uniform(0.5, 3.0), 1))
-        decel = str(round(uniform(4.0, 6.0), 1))
-        id = 'Car'+str(i)
-        length = str(round(uniform(10, 25), 1))
-        maxSpeed = str(randint(60, 120))
-        sigma = '0.0'
-        vType = SubElement(
-            routes, 'vType', {'accel':accel, 'decel':decel, 'id':id, 'length':length, 'maxSpeed':maxSpeed, 'sigma':sigma})
-
-    first = path[0]
-    full_route = ''
-    for i in range(1, len(path)):
-        from_ = str(first)
-        to_ = str(path[i])
-        id = from_ + '_' + to_
-        full_route += id + ' '
-        first = path[i]
-    route = SubElement(routes, 'route', {'id':'route0', 'edges':full_route[:-1]})
-    tree = ET.ElementTree(routes)
-    tree.write(file_name + ".rou.xml")
-
-
-    begin = "0"
-    departPos = "0"
-    id = '0'
-    period = "1"
-    number = str(num_cars_in_flow)
-    route = 'route0'
-    type = "Car"+str(randint(0,num_vehicle_types))
-    flow = SubElement(
-        routes, 'flow',
-        {'begin':begin, 'departPos':departPos, 'id':id, 'period':period, 'number':number, 'route':route, 'type':type})
-
-    xml_ = xml.dom.minidom.parseString(tostring(routes))
-    print(xml_.toprettyxml())
-
-def multiple_route_vehicles_xml(paths, num_vehicle_types, num_cars, is_shortest_path, file_name):
+def multiple_route_vehicles_xml(paths, num_vehicle_types, num_cars, num_back_cars, is_shortest_path, file_name):
     num_cars_in_flow = int(num_cars/len(paths))
+    num_cars_in_back_flow = int(num_back_cars / len(paths))
     routes = Element('routes')
 
     for i in range(num_vehicle_types):
@@ -115,6 +57,9 @@ def multiple_route_vehicles_xml(paths, num_vehicle_types, num_cars, is_shortest_
         sigma = str(uniform(0.0, 1.0))
         vType = SubElement(
             routes, 'vType', {'accel':accel, 'decel':decel, 'id':id, 'length':length, 'maxSpeed':maxSpeed, 'sigma':sigma})
+    vType = SubElement(
+        routes, 'vType',
+        {'accel': accel, 'decel': decel, 'id': 'back_car', 'length': length, 'maxSpeed': maxSpeed, 'sigma': sigma})
 
     for i, path in enumerate(paths):
         first = path[0]
@@ -134,9 +79,10 @@ def multiple_route_vehicles_xml(paths, num_vehicle_types, num_cars, is_shortest_
         id = str(i)
         period = "1"
         number = str(num_cars_in_flow)
+        num_back = str(num_cars_in_back_flow)
         route = 'route'+str(i)
         if is_shortest_path:
-            num_flows_for_shortest_path = 10
+            num_flows_for_shortest_path = 20
             number = str(int(num_cars_in_flow/num_flows_for_shortest_path))
             for n in range(num_flows_for_shortest_path):
                 id = str(n)
@@ -145,27 +91,32 @@ def multiple_route_vehicles_xml(paths, num_vehicle_types, num_cars, is_shortest_
                     routes, 'flow',
                     {'begin':begin, 'departPos':departPos, 'id':id,
                      'period':period, 'number':number, 'route':route, 'type':type})
+            flow = SubElement(
+                routes, 'flow',
+                {'begin': begin, 'departPos': departPos, 'id': 'back_flow',
+                 'period': period, 'number': str(num_back_cars), 'route': route, 'type': 'back_car'})
         else:
             type = "Car" + str(randint(0, num_vehicle_types-1))
             flow = SubElement(
                 routes, 'flow',
                 {'begin': begin, 'departPos': departPos, 'id': id,
                  'period': period, 'number': number, 'route': route, 'type': type})
+            id_back = str(i)
+            flow = SubElement(
+                routes, 'flow',
+                {'begin': begin, 'departPos': departPos, 'id': 'back_flow'+id_back,
+                 'period': period, 'number': num_back, 'route': route, 'type': 'back_car'})
+
+
+
 
     tree = ET.ElementTree(routes)
     tree.write(file_name + ".rou.xml")
 
-    # xml_ = xml.dom.minidom.parseString(tostring(routes))
-    # print(xml_.toprettyxml())
-
-def all_xmls(graph, paths, num_vehicle_types, num_cars, is_shortest_path, file_name):
+def all_xmls(graph, paths, num_vehicle_types, num_cars, num_cars_in_back, is_shortest_path, file_name):
     nodes_xml(graph, paths, file_name)
-    if type(paths[0]) == int:
-        single_edges_xml(paths, file_name)
-        single_route_vehicles_xml(paths, num_vehicle_types, num_cars, file_name)
-    else:
-        multiple_edges_xml(paths, file_name)
-        multiple_route_vehicles_xml(paths, num_vehicle_types, num_cars, is_shortest_path, file_name)
+    multiple_edges_xml(paths, file_name)
+    multiple_route_vehicles_xml(paths, num_vehicle_types, num_cars, num_cars_in_back, is_shortest_path, file_name)
 
 
 
